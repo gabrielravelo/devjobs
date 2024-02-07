9f919addb807a0ae67486a240d58a29fa875ecaf
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-02-06 23:21:51 -0400

    breeze + register/login/verify + campo rol en users

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Por favor haz click en el botón de abajo para verificar tu dirección de email.')
+                ->action('Confirmar Cuenta', $url)
+                ->line('Si no creaste una cuenta, ignora este mensaje.');
+        });
     }
 }
---
+++ resources/views/auth/verify-email.blade.php
@@ -0,0 +1,31 @@
+<x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600">
+        {{ __('Gracias por registrarte! Antes de empezar, podrías verificar tu dirección de email haciendo click en el link que acabamos de enviarte? Si no recibiste el email, estaremos encantados de enviarte otro.') }}
+    </div>
+
+    @if (session('status') == 'verification-link-sent')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ __('Un nuevo link ha sido enviado a la dirección de email suministrada al registrarte.') }}
+        </div>
+    @endif
+
+    <div class="mt-4 flex items-center justify-between">
+        <form method="POST" action="{{ route('verification.send') }}">
+            @csrf
+
+            <div>
+                <x-primary-button>
+                    {{ __('Enviar Email de Confirmación') }}
+                </x-primary-button>
+            </div>
+        </form>
+
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <button type="submit" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                {{ __('Cerrar Sesión') }}
+            </button>
+        </form>
+    </div>
+</x-guest-layout>
