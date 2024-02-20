6684cbaa69ddf73308d16fbc8559879d40c8f4d4
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-02-19 22:43:41 -0400

    mostrar vacante + permisos de roles

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -12,6 +12,8 @@ class VacanteController extends Controller
      */
     public function index()
     {
+        $this->authorize('viewAny', Vacante::class);
+
         return view('vacantes.index');
     }
 
@@ -20,15 +22,19 @@ class VacanteController extends Controller
      */
     public function create()
     {
+        $this->authorize('create', Vacante::class);
+        
         return view('vacantes.create');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Vacante $vacante)
     {
-        //
+        return view('vacantes.show', [
+            'vacante' => $vacante
+        ]);
     }
 
     /**
